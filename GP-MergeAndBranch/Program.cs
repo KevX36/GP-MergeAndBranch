7f10f40ce0415cd137f00cdfef5520f180002d85
KevX36 <kevincard2007@gmail.com>
2025-10-27 09:54:26 -0300

    idk what to say here

--- GP-MergeAndBranch/Program.cs
+++ GP-MergeAndBranch/Program.cs
@@ -11,6 +11,7 @@ namespace GP_MergeAndBranch
         static void Main(string[] args)
         {
             Console.WriteLine("This is a line");
+            Console.WriteLine("yes it is");
         }
     }
 }
